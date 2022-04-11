36f084272cc5b1ce3243fbb31739f82642a8ee67
Unknown <mr.i.d.fernando@gmail.com>
2022-04-10 22:31:51 -0500

    Fixes draw polygon function

--- src/utils/canvasutils.js
+++ src/utils/canvasutils.js
@@ -6,8 +6,9 @@ export const setupCanvas = (canvasId, width, height) => {
   return { canvasContext: canvas.getContext('2d'), canvas }
 }
 
-export const drawPolygon = (canvasCtx, points, color) => {
+export const drawPolygon = (canvasCtx, points, lineWidth, color) => {
   canvasCtx.strokeStyle = color
+  canvasCtx.lineWidth = lineWidth
   canvasCtx.beginPath()
   canvasCtx.moveTo(points[0].x, points[0].y)
   for (let i = 1; i < points.length; i++) {
